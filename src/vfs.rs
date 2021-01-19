a685ec6b3ec3329c763be0fa0784c748ffab376c
Prabir Shrestha <mail@prabir.me>
2021-01-19 04:03:51 +0000

    add vfs.remove_dir(path) -> VfsResult<()>

--- src/vfs.rs
+++ src/vfs.rs
@@ -7,4 +7,5 @@ pub trait Vfs {
     fn path_separator(&self) -> char;
 
     async fn create_dir(&self, path: &str) -> VfsResult<()>;
+    async fn remove_dir(&self, path: &str) -> VfsResult<()>;
 }
