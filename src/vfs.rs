b164c3ceb4e74d34a22a9f534be32ec3e6a180f6
Prabir Shrestha <mail@prabir.me>
2021-01-19 03:59:11 +0000

    add vfs.creat_dir(path) -> Result<()>

--- src/vfs.rs
+++ src/vfs.rs
@@ -1,6 +1,10 @@
 use async_trait::async_trait;
 
+use crate::VfsResult;
+
 #[async_trait]
 pub trait Vfs {
-    fn path_separator() -> char;
+    fn path_separator(&self) -> char;
+
+    async fn create_dir(&self, path: &str) -> VfsResult<()>;
 }
