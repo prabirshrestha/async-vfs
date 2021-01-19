8e858347affd899a43cd338b495da248b3e870c7
Prabir Shrestha <mail@prabir.me>
2021-01-19 04:08:29 +0000

    add vfs.metadata(path) -> VfsResult<VfsMetadata>

--- src/vfs.rs
+++ src/vfs.rs
@@ -2,6 +2,8 @@ use async_trait::async_trait;
 
 use crate::VfsResult;
 
+pub struct VfsMetadata {}
+
 #[async_trait]
 pub trait Vfs {
     fn path_separator(&self) -> char;
@@ -11,4 +13,6 @@ pub trait Vfs {
 
     async fn remove_file(&self, path: &str) -> VfsResult<()>;
     async fn rename(&self, from: &str, to: &str) -> VfsResult<()>;
+
+    async fn metadata(&self, path: &str) -> VfsResult<VfsMetadata>;
 }
