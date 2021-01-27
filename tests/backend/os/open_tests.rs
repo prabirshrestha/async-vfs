88aa751bca7bca4d315a568200d4bedbd83ca785
Prabir Shrestha <mail@prabir.me>
2021-01-27 04:49:36 +0000

    add more tests for open read

--- tests/backend/os/open_tests.rs
+++ tests/backend/os/open_tests.rs
@@ -7,14 +7,26 @@ use async_vfs::*;
 async fn open_read_only() -> VfsResult<()> {
     let vfs = OsFs::new(&data_dir());
 
+    let mut buf = String::new();
     let mut file = vfs
         .open("/file1a.txt", OpenOptions::new().read(true))
         .await?;
+    file.read_to_string(&mut buf).await?;
+    assert_eq!(buf.as_str(), "contents for file 1 a in /\n");
 
     let mut buf = String::new();
+    let mut file = vfs
+        .open("/dir1/empty.txt", OpenOptions::new().read(true))
+        .await?;
     file.read_to_string(&mut buf).await?;
+    assert!(true, buf.is_empty());
 
-    assert_eq!(buf.as_str(), "contents for file 1 a in /\n");
+    let mut buf = String::new();
+    let mut file = vfs
+        .open("/dir1/filed1a.txt", OpenOptions::new().read(true))
+        .await?;
+    file.read_to_string(&mut buf).await?;
+    assert_eq!(buf.as_str(), "filed1a.txt here\n");
 
     Ok(())
 }
