a9d68652c644c222aa3174c521d18cee8263df15
Prabir Shrestha <mail@prabir.me>
2021-01-27 04:13:54 +0000

    use test utils data_dir for exists test

--- tests/backend/os/exists_tests.rs
+++ tests/backend/os/exists_tests.rs
@@ -1,43 +1,42 @@
+use crate::testutils::data_dir;
 use async_vfs::backend::OsFs;
 use async_vfs::*;
 
-const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
-
 #[async_std::test]
 async fn exists_true_for_existing_file() -> VfsResult<()> {
-    let vfs = OsFs::new(CARGO_MANIFEST_DIR);
+    let vfs = OsFs::new(&data_dir());
 
-    assert_eq!(vfs.exists("/Cargo.toml").await?, true);
-    assert_eq!(vfs.exists("/src/lib.rs").await?, true);
+    assert_eq!(vfs.exists("/file1a.txt").await?, true);
+    assert_eq!(vfs.exists("/dir1/filed1a.txt").await?, true);
 
     Ok(())
 }
 
 #[async_std::test]
 async fn exists_true_for_existing_dir() -> VfsResult<()> {
-    let vfs = OsFs::new(CARGO_MANIFEST_DIR);
+    let vfs = OsFs::new(&data_dir());
 
-    assert_eq!(vfs.exists("/src").await?, true);
-    assert_eq!(vfs.exists("/src/backend").await?, true);
+    assert_eq!(vfs.exists("/dir1").await?, true);
+    assert_eq!(vfs.exists("/dir2/dir3").await?, true);
 
     Ok(())
 }
 
 #[async_std::test]
 async fn exists_false_for_non_existent_file() -> VfsResult<()> {
-    let vfs = OsFs::new(CARGO_MANIFEST_DIR);
+    let vfs = OsFs::new(&data_dir());
 
     assert_eq!(vfs.exists("/nonexistent.file").await?, false);
-    assert_eq!(vfs.exists("/src/nonexistent.file").await?, false);
+    assert_eq!(vfs.exists("/dir1/nonexistent.file").await?, false);
 
     Ok(())
 }
 
 #[async_std::test]
 async fn exists_fail_when_using_path_without_forward_slash_prefix() -> VfsResult<()> {
-    let vfs = OsFs::new(CARGO_MANIFEST_DIR);
+    let vfs = OsFs::new(&data_dir());
 
-    match vfs.exists("Cargo.toml").await {
+    match vfs.exists("file1a.txt").await {
         Err(_) => assert!(true),
         _ => assert!(false, "should throw VfsError::InvalidAbsolutePath"),
     }
@@ -52,14 +51,14 @@ async fn exists_fail_when_using_path_without_forward_slash_prefix() -> VfsResult
 
 #[async_std::test]
 async fn exists_fail_when_include_dotdot() -> VfsResult<()> {
-    let vfs = OsFs::new(CARGO_MANIFEST_DIR);
+    let vfs = OsFs::new(&data_dir());
 
     match vfs.exists("/../nonexistent.file").await {
         Err(_) => assert!(true),
         _ => assert!(false, "should throw VfsError::InvalidAbsolutePath"),
     }
 
-    match vfs.exists("/src/../nonexistent.file").await {
+    match vfs.exists("/dir1/../nonexistent.file").await {
         Err(_) => assert!(true),
         _ => assert!(false, "should throw VfsError::InvalidAbsolutePath"),
     }
