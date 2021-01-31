d305b4209901acfa80111bc29949ab28d50204d3
Prabir Shrestha <mail@prabir.me>
2021-01-31 23:21:31 +0000

    add test for ls non root

--- tests/backend/os/ls_tests.rs
+++ tests/backend/os/ls_tests.rs
@@ -32,7 +32,34 @@ async fn ls_root() -> VfsResult<()> {
                 assert_eq!(entry.is_file(), true);
                 assert_eq!(entry.len(), 0);
             }
-            _ => {}
+            _ => unreachable!(),
+        }
+    }
+
+    Ok(())
+}
+
+#[async_std::test]
+async fn ls_non_root() -> VfsResult<()> {
+    let vfs = OsFs::new(&data_dir());
+
+    let (entries, skip_token) = vfs.ls("/dir2", None).await?;
+    assert_eq!(skip_token, None);
+    assert_eq!(entries.len(), 2);
+
+    for entry in entries {
+        match entry.path() {
+            "/dir2/dir3" => {
+                assert_eq!(entry.is_dir(), true);
+                assert_eq!(entry.is_file(), false);
+                assert_eq!(entry.len(), 0);
+            }
+            "/dir2/filed2a.txt" => {
+                assert_eq!(entry.is_dir(), false);
+                assert_eq!(entry.is_file(), true);
+                assert_eq!(entry.len(), 0);
+            }
+            _ => unreachable!(),
         }
     }
 
