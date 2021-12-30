c157e64d0c57c7ca7dda63b4d255c8fb968a79da
Prabir Shrestha <mail@prabir.me>
2021-12-30 22:04:11 +0000

    fix mkdir_ok tests

--- tests/backend/os/mkdir_tests.rs
+++ tests/backend/os/mkdir_tests.rs
@@ -3,7 +3,6 @@ use async_vfs::backend::OsFs;
 use async_vfs::*;
 
 #[async_test]
-#[ignore]
 async fn mkdir_ok() -> VfsResult<()> {
     let vfs = OsFs::new(&data_dir());
 
@@ -20,16 +19,14 @@ async fn mkdir_ok() -> VfsResult<()> {
 
     let path = "/dir2/mkdir2";
     assert_eq!(vfs.exists(path).await?, false);
-    vfs.mkdir("/dir2").await?;
     vfs.mkdir(path).await?;
     assert_eq!(vfs.exists(path).await?, true);
     let metadata = vfs.metadata(path).await?;
     assert_eq!(metadata.is_dir(), true);
     assert_eq!(metadata.is_file(), false);
     assert_eq!(metadata.len(), 0);
-    assert_eq!(metadata.path(), "/dir2/mkdir2");
+    assert_eq!(metadata.path(), path);
     vfs.rm(path).await?;
-    vfs.rm("/dir2").await?;
     Ok(())
 }
 
